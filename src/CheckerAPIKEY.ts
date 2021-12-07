28bd673b1cad357ef91180c2a1ddfc241373a645
Sora210 <sokominto1231@gmail.com>
2021-12-07 15:41:47 +0900

    [fix] token取得部分の修正

--- src/CheckerAPIKEY.ts
+++ src/CheckerAPIKEY.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 
 const APIChecker = function(req, res, next) {
-    if (req.header['Authorization'] == "testApiKey") {
+    const token = req.header('Authorization')
+    if (token == "cQFZkLQLSDBbPrshFD6G") {
         next()
     } else {
         res.status(403).json({"message":"API error"})
