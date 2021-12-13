f022c9d9c53512f819c2f4b86cb03280def3998e
Sora210 <sokominto1231@gmail.com>
2021-12-13 13:35:28 +0900

    [update] Room関連の処理修正, httpメソッドの変更

--- src/router/RoomInOut.ts
+++ src/router/RoomInOut.ts
@@ -2,6 +2,12 @@ import { Router } from 'express'
 const RoomInOut = Router()
 
 import {DB} from '../db/index'
+import {Msg} from '../Msg'
+
+import { NonNullFindOptions } from 'sequelize'
+interface order_find_option extends NonNullFindOptions{
+    order?: any
+}
 
 /*------------
 get /room
@@ -16,200 +22,236 @@ get /room/history/:userId
 RoomInOut.get('/', async(req, res) => {
     const T = DB.instance.transaction();
     try {
-        const option = {
+        const History = await DB.RoomInOuts.findAll({
             where: {
                 leaveTime: null
             }
-        }
-        const History = await DB.RoomInOuts.findAll(option);
+        });
         (await T).commit();
         res.status(200).json(History)
     } catch(e) {
         (await T).rollback();
-        const resMes = {
-            "status": "Error",
-            "message": "NotFound"
+        console.log("###########################")
+        console.error(e)
+        res.status(500).json(Msg.ServerError);
+    }
+})
+
+RoomInOut.get('/history', async (req, res) => {
+    console.log("1")
+    const T = DB.instance.transaction();
+    try {
+        const option:order_find_option = {
+            order: [
+                ['id', 'DESC']
+            ],
+            rejectOnEmpty: false
         }
-        res.status(404).json(resMes)
+        const Historys = await DB.RoomInOuts.findAll(option);
+        (await T).commit();
+        res.status(200).json(Historys)
+    } catch(e) {
+        (await T).rollback();
+        console.log("###########################")
+        console.error(e)
+        res.status(500).json(Msg.ServerError);
+    }
+})
+
+
+RoomInOut.get('/history/:userId', async(req, res) => {
+    console.log("2")
+    if (!req.params.userId) {
+        res.status(402).json(Msg.NotEnoughProp);
+        return 0
+    }
+    const T = DB.instance.transaction();
+    try {
+        const option:order_find_option = {
+            where: {
+                userId: req.params.userId
+            },
+            order: [
+                ['id', 'DESC']
+            ],
+            rejectOnEmpty: false
+        }
+        const Historys = await DB.RoomInOuts.findAll(option);
+        (await T).commit();
+        res.status(200).json(Historys)
+    } catch(e) {
+        (await T).rollback();
+        console.log("###########################")
+        console.error(e)
+        res.status(500).json(Msg.ServerError);
     }
 })
 
 
 RoomInOut.get('/:userId', async (req, res) => {
+    if (!req.params.userId) {
+        res.status(402).json(Msg.NotEnoughProp);
+        return 0
+    }
     const T = DB.instance.transaction();
     try {
-        const option = {
+        const isUserRoom = await DB.RoomInOuts.findOne({
             where: {
                 userId: req.params.userId,
                 leaveTime: null
             }
+        });
+        const Res = {
+            "userId": req.params.userId,
+            "status": "out",
+            "enterTime": null
         }
-        const History = await DB.RoomInOuts.findOne(option);
+        if (isUserRoom) {
+            Res.status = "in"
+            Res.enterTime = isUserRoom.enterTime
+        }
         (await T).commit();
-        res.status(200).json(History)
+        res.status(200).json(Res)
     } catch(e) {
         (await T).rollback();
-        const resMes = {
-            "status": "Error",
-            "message": "NotFound"
-        }
-        res.status(404).json(resMes)
+        console.log("###########################")
+        console.error(e)
+        res.status(500).json(Msg.ServerError);
     }
 })
 
-import { NonNullFindOptions } from 'sequelize'
-interface last_find_option extends NonNullFindOptions{
-    order?: any
-}
 
 RoomInOut.patch('/:userId', async (req, res) => {
+    if (!req.params.userId) {
+        res.status(402).json(Msg.NotEnoughProp);
+        return 0
+    }
     const T = DB.instance.transaction()
-    if (req.params.userId == "") {
-        const resMes = {
-            status: "Error",
-            message: "Not Enough Params"
-        }
-        res.status(402).json(resMes);
-    } else {
-        try {
-            const nowTime = Date.now()
+    try {
+        const nowTime = Date.now()
 
-            const option = {
-                where: {
-                    userId: req.params.userId,
-                    leaveTime: null
-                }
+        const isOldHistory = await DB.RoomInOuts.findOne({
+            where: {
+                userId: req.params.userId,
+                leaveTime: null
             }
-            const oldHistory = await DB.RoomInOuts.findOne(option);
-            //入室済みのデータがある場合退出処理を行って再度入室処理を行う
-            if (oldHistory != null) {
-                const sendData = {
-                    leaveTime: nowTime
-                }
-                //退出処理
-                await DB.RoomInOuts.update(sendData, option)
-            } else {
-                //入室済みデータがない場合は入室処理
-                const sendData = {
-                    userId: req.params.userId,
-                    enterTime: nowTime
-                }
-                await DB.RoomInOuts.create(sendData)
-            }
+        });
 
-            //最終的な現在状況を取得
-            const last_option:last_find_option = {
+        //入室済みのデータがある場合退出処理を行って再度入室処理を行う
+        if (isOldHistory) { 
+            //退出処理
+            await DB.RoomInOuts.update({
+                leaveTime: nowTime
+            }, {
                 where: {
-                    userId: req.params.userId
-                },
-                order: [
-                    ['id', 'DESC']
-                ],
-                rejectOnEmpty: true
-            }
-            const History = await DB.RoomInOuts.findOne(last_option);
-            (await T).commit();
-            res.status(200).json(History)
-        } catch (e) {
-            (await T).rollback();
-            const resMes = {
-                "status": "Error",
-                "message": "ServerError"
-            }
-            res.status(500).json(resMes)
-            console.log(e);
+                    id: isOldHistory.id
+                }
+            })
+        } else {
+            //入室済みデータがない場合は入室処理
+            await DB.RoomInOuts.create({
+                userId: req.params.userId,
+                enterTime: nowTime
+            })
         }
+
+        //最終的な現在状況を取得
+        const last_option:order_find_option = {
+            where: {
+                userId: req.params.userId,
+                leaveTime: null
+            },
+            order: [
+                ['id', 'DESC']
+            ],
+            rejectOnEmpty: false
+        }
+        const isUserRoom = await DB.RoomInOuts.findOne(last_option);
+        const Res = {
+            "userId": req.params.userId,
+            "status": "out",
+            "enterTime": null
+        }
+        if (isUserRoom) {
+            Res.status = "in"
+            Res.enterTime = isUserRoom.enterTime
+        }
+        (await T).commit();
+        res.status(200).json(Res)
+    } catch (e) {
+        (await T).rollback();
+        console.log("###########################")
+        console.error(e)
+        res.status(500).json(Msg.ServerError);
     }
 })
 
 
 RoomInOut.post('/:userId', async (req, res) => {
     const T = DB.instance.transaction()
-    if (req.params.userId == "") {
-        const resMes = {
-            status: "Error",
-            message: "Not Enough Params"
-        }
-        res.status(402).json(resMes);
-    } else {
-        try {
-            const nowTime = Date.now()
-
-            const option = {
-                where: {
-                    userId: req.params.userId,
-                    leaveTime: null
-                }
-            }
-            const oldHistory = await DB.RoomInOuts.findOne(option);
-            console.log(oldHistory);
-            //入室済みのデータがある場合退出処理を行って再度入室処理を行う
-            if (oldHistory != null) {
-                const sendData = {
-                    leaveTime: nowTime
-                }
-                //退出処理
-                await DB.RoomInOuts.update(sendData, option)
-            }
-            if (req.body.status == "in") {
-                //入室処理
-                const sendData = {
-                    userId: req.params.userId,
-                    enterTime: nowTime
-                }
-                await DB.RoomInOuts.create(sendData)
-            }
-            //最終的な現在状況を取得
-            const History = await DB.RoomInOuts.findOne(option);
-            (await T).commit();
-            res.status(200).json(History)
-        } catch (e) {
-            (await T).rollback();
-            const resMes = {
-                "status": "Error",
-                "message": "ServerError"
-            }
-            res.status(500).json(resMes)
-        }
+    if (!req.params.userId || !req.body.status) {
+        res.status(402).json(Msg.NotEnoughProp);
+        return 0
     }
-})
-
-
-RoomInOut.get('/history', async (req, res) => {
-    const T = DB.instance.transaction();
     try {
-        const History = await DB.RoomInOuts.findAll();
-        (await T).commit();
-        res.status(200).json(History)
-    } catch(e) {
-        (await T).rollback();
-        const resMes = {
-            "status": "Error",
-            "message": "NotFound"
-        }
-        res.status(404).json(resMes)
-    }
-})
+        const nowTime = Date.now()
 
-
-RoomInOut.get('/history/:userId', async(req, res) => {
-    const T = DB.instance.transaction();
-    try {
-        const config = {
+        const isOldHistory = await DB.RoomInOuts.findOne({
             where: {
-                userId: req.params.userId
+                userId: req.params.userId,
+                leaveTime: null
             }
+        });
+
+        //入室済みのデータがある場合退出処理を行って再度入室処理を行う
+        if (isOldHistory) {
+            //退出処理
+            await DB.RoomInOuts.update({
+                leaveTime: nowTime
+            }, {
+                where: {
+                    id: isOldHistory.id
+                }
+            })
         }
-        const History = await DB.RoomInOuts.findAll(config);
+
+        if (req.body.status == "in") {
+            //入室処理
+            const sendData = {
+                userId: req.params.userId,
+                enterTime: nowTime
+            }
+            await DB.RoomInOuts.create(sendData)
+        }
+
+        //最終的な現在状況を取得
+        const last_option:order_find_option = {
+            where: {
+                userId: req.params.userId,
+                leaveTime: null
+            },
+            order: [
+                ['id', 'DESC']
+            ],
+            rejectOnEmpty: false
+        }
+        const isRoomStatus = await DB.RoomInOuts.findOne(last_option);
+        const Res = {
+            "userId": req.params.userId,
+            "status": "out",
+            "enterTime": null
+        }
+        if (isRoomStatus) {
+            Res.status = "in"
+            Res.enterTime = isRoomStatus.enterTime
+        }
         (await T).commit();
-        res.status(200).json(History)
-    } catch(e) {
+        res.status(200).json(Res)
+    } catch (e) {
         (await T).rollback();
-        const resMes = {
-            "status": "Error",
-            "message": "NotFound"
-        }
-        res.status(404).json(resMes)
+        console.log("###########################")
+        console.error(e)
+        res.status(500).json(Msg.ServerError);
     }
 })
 
